0758f2a8f49c05da9a69b33d9abce53b0795e33c
Magnaldo Melo <magnaldo.melo@gmail.com>
2022-07-01 01:13:43 -0300

    Finish

--- src/models/fotos.ts
+++ src/models/fotos.ts
@@ -1,3 +1,12 @@
+
+//Fotos
+/**
+* Model de fotos
+* @type thumb {String} foto principal
+* @type thumbNail {String} foto menor
+* @type credito {String} quem tirou a foto
+* @type legenda {String} descrição da foto
+*/
 export class Fotos {
     thumb: String;
     thumbNial: String;
--- src/startUp.ts
+++ src/startUp.ts
@@ -1,22 +1,15 @@
-import "reflect-metadata";
-import { NewsController } from "./controller/newsController";
-import { VideosController } from "./controller/videosController";
-import { GaleriaController } from "./controller/galeriaController"
-import { container } from 'tsyringe';
-import './shared/container';
-
-import "reflect-metadata";
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import Database from "./infra/db";
+
+import './shared/container';
+import newsRouter from "./router/newsRouter";
+import videosRouter from "./router/videosRouter";
+import galeriaRouter from "./router/galeriaRouter";
 
 class StartUp {
 
     public app:	Application;
     private	_db: Database =	new	Database();
-
-    private news = container.resolve(NewsController);
-    private videos = container.resolve(VideosController);
-    private galeria = container.resolve(GaleriaController);
 
     constructor() {
         this.app = express();
@@ -26,35 +19,12 @@ class StartUp {
 
     routes() {
         this.app.route("/").get((req, res) => {
-            res.send({ versao: "0.0.1" });
-        });
-        
-        /*news*/
-        this.app.route("/api/v1/news/:page/:qtd").get((req:	Request, res: Response) => {
-            return this.news.get(req, res);
+            res.send({ versao: "0.0.2" });
         });
 
-        this.app.route("/api/v1/news/:id").get((req: Request, res: Response) =>	{
-            return	this.news.getById(req,	res);
-        });
-
-        /*videos*/
-        this.app.route("/api/v1/videos/:page/:qtd").get((req: Request, res: Response) => {
-            return this.videos.get(req, res);
-        });
-    
-        this.app.route("/api/v1/videos/:id").get((req: Request, res: Response) => {
-            return this.videos.getById(req, res);
-        });
-
-        /*galeria*/
-        this.app.route("/api/v1/galeria/:page/:qtd").get((req: Request, res: Response) => {
-            return this.galeria.get(req, res);
-        });
-
-        this.app.route("/api/v1/galeria/:id").get((req: Request, res: Response) => {
-            return this.galeria.getById(req, res);
-        });
+        this.app.use("/", newsRouter);
+        this.app.use("/", videosRouter);
+        this.app.use("/", galeriaRouter);
     }
 }
 
